d05157ecf9460eb8fe1ebbfdd84ee3e43ce44b34
Ryan England <raenglandwork@gmail.com>
2024-11-12 15:50:08 -0800

    Added plugins and app title

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-// todo
+// @deno-types="npm:@types/leaflet@^1.9.14"
+import leaflet from "leaflet";
+
+// Style sheets
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+
+// Fix missing marker images
+import "./leafletWorkaround.ts";
+
+// Location of our classroom (as identified on Google Maps)
+const _OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
+const APP_NAME = "Hello, welcome to our Canvas";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+
+document.title = APP_NAME;
+app.innerHTML = APP_NAME;
+
+const map = document.createElement("div");
+map.id = "map";
+map.offsetHeight;
